48ae961f470d4da96ada55dc486552aefa610809
ronakjp <ronak.p@thinkwik.com>
2024-07-31 14:54:48 +0530

    added the header element styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { DiGitCompare } from "react-icons/di";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className=" flex h-40 w-full p-1 border-b-2 border-solid border-slate-700 ">
       <div className="flex items-center justify-between w-full">
@@ -15,8 +17,22 @@ const Header = () => {
         </div>
         <div className="w-80">
           <ul className="flex justify-evenly gap-3  text-base text-orange-500 ">
-            <li>Home</li>
-            <li>Login</li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? "text-black" + " underline" : ""
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to={"/login"}
+              className={({ isActive }) =>
+                isActive ? "text-black" + " underline" : ""
+              }
+            >
+              Login
+            </NavLink>
           </ul>
         </div>
       </div>
